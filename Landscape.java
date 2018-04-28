2f94605c170f1debcb92713c11e2d1eea5c1f608
Wallace He <guanlin.he@richmond.edu>
2018-04-28 14:35:29 -0400

    update the landscpe
    update the landscpe, especially add the gaussian inside

--- Landscape.java
+++ Landscape.java
@@ -1,6 +1,10 @@
+package abs;
+
+import java.util.Random;
+
 public class Landscape
 {
-	private int MAX_DIMENSION;
+	private int MAX_DIMENSION = 40;
 	private Cell [][] landscape; // instance of the landscape 2x2 array
 
 /*	public Landscape()
@@ -14,15 +18,23 @@ public class Landscape
 
 		for(int i = 0; i < row; i++) {
 			for(int j = 0; j < col; j++) {
-				landscape[i][j] = new Cell(10, 0.75, false, 4);
+				landscape[i][j] = new Cell(getGaussian(i,j), 0.75, false, 4);
 			}
 		}
 	}
 
-//	public void setCell(int row, int col)
-//	{
-//		landscape[row][col] = new Cell
-//	}
+
+        
+        
+	public double getGaussian(int x, int y) {
+    return gaussianHelper(x - MAX_DIMENSION/4, y - MAX_DIMENSION/4) + gaussianHelper(x - 3*MAX_DIMENSION/4, y - 3*MAX_DIMENSION/4);
+  }
+
+	private double gaussianHelper(double x, double y) {
+    double thetaX = 0.4 * MAX_DIMENSION;
+    double thetaY = 0.4 * MAX_DIMENSION;
+    return Math.pow(4.0, - ((x/thetaX)*(x/thetaX)) - ((y/thetaY)*(y/thetaY)));
+  }
 
 	public Cell getCellAt(int row, int col)
 	{
